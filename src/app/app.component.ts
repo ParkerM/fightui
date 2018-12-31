705f3a7e42a82138a21239537a2a0a189fd07f5d
Parker Mauney <parkermauney@gmail.com>
2018-12-31 11:13:35 -0600

    Half working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {Subject} from 'rxjs';
+import {Fight} from './services/fight.model';
+import {FightstoreService} from './services/fightstore.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'fightui';
+
+  datarray: Fight[] = [];
+  dataSubject: Subject<Fight>;
+
+  constructor(private fightstoreService: FightstoreService) {
+    this.dataSubject = new Subject<Fight>();
+  }
+
+  ngAfterViewInit(): void {
+    this.fightstoreService.fetchData().subscribe((fight: Fight) => {
+      this.datarray.push(fight);
+      this.dataSubject.next(fight);
+    });
+  }
 }
