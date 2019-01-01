36b66b9eede5a9e25403eb62846d1c90390c063a
Parker Mauney <parkermauney@gmail.com>
2018-12-31 19:03:09 -0600

    wiptestos

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -84,4 +84,43 @@ describe('AppComponent', () => {
     mockFightSubject.next(fights[2]);
     fixture.detectChanges();
   }));
+
+  describe('time-based stuff', () => {
+    let myDate: Date;
+    let timeyFights: Fight[];
+
+    beforeEach(() => {
+      myDate = new Date(2018, 7, 19, 14, 18, 25);
+      jest.spyOn(Date, 'now').mockImplementation(() => myDate);
+
+      timeyFights = [
+        // 13 days and 16 or so hours ahead (2018-8-2 08:05:05)
+        new Fight({eventDate: 1533197105000, eventName: 'King fight', _id: 'fff0000000000', _timestamp: 90909090}),
+
+        // 2 days, 2 hours and 10 minutes ahead (2018-7-21 16:28:25)
+        new Fight({eventDate: 1532190505000, eventBout: 'Bout bout bout v bout', _id: 'bbbbeeeeeeeeffff', eventOrg: 'MooFC'}),
+
+        // 4 days past (2018-7-15 14:18:25)
+        new Fight({eventDate: 1531664305000, eventBilling: 'check please', _id: 'ffff000000000dddd', eventOrg: 'The Guild of Iron Chefs'}),
+      ];
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('displays time remaining on soonest future event', async(() => {
+      const nextFightTimeMs = 1532190505000;
+      const nextFightDate = new Date(nextFightTimeMs);
+      const nextFightTimeDiff = nextFightDate.getUTCMilliseconds() - myDate.getUTCMilliseconds();
+
+      fixture.detectChanges();
+      mockFightSubject.next(timeyFights[0]);
+      mockFightSubject.next(timeyFights[1]);
+      mockFightSubject.next(timeyFights[2]);
+      fixture.detectChanges();
+
+      expect(el.textContent).toContain('2d 02h 10m 00s');
+    }));
+  });
 });
