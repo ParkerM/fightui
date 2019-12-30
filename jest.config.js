fac1631f15f852914c39763ec68b464e25ce0a06
Parker Mauney <parkermauney@gmail.com>
2019-12-30 14:55:01 -0600

    Reconfigure jest with @briebug/jest-schematic

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "jest-preset-angular",
-  roots: ['src', 'server'],
-  setupFilesAfterEnv: ["<rootDir>/src/setupJest.ts"],
-  coveragePathIgnorePatterns: [
-    "<rootDir>/src/jestGlobalMocks.ts"
-  ]
-};
---
+++ src/setup-jest.ts
@@ -0,0 +1,38 @@
+import 'jest-preset-angular';
+
+/**
+ * Some components (mat-slide-toggle, mat-slider, matTooltip) rely on HammerJS for gestures.
+ * In order to get the full feature-set of these components, HammerJS must be loaded into the application.
+ * Fix for: The "longpress" event cannot be bound because Hammer.JS is not loaded and no custom loader has been specified.
+ * https://material.angular.io/guide/getting-started#step-5-gesture-support
+ */
+import 'hammerjs';
+
+/* global mocks for jsdom */
+const mock = () => {
+  let storage: { [key: string]: string } = {};
+  return {
+    getItem: (key: string) => (key in storage ? storage[key] : null),
+    setItem: (key: string, value: string) => (storage[key] = value || ''),
+    removeItem: (key: string) => delete storage[key],
+    clear: () => (storage = {})
+  };
+};
+
+Object.defineProperty(window, 'localStorage', { value: mock() });
+Object.defineProperty(window, 'sessionStorage', { value: mock() });
+Object.defineProperty(window, 'getComputedStyle', {
+  value: () => ['-webkit-appearance'],
+});
+
+Object.defineProperty(document.body.style, 'transform', {
+  value: () => {
+    return {
+      enumerable: true,
+      configurable: true,
+    };
+  },
+});
+
+/* output shorter and more meaningful Zone error stack traces */
+// Error.stackTraceLimit = 2;
